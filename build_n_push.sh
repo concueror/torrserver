e07d592ec41318d124a83517d9eeee6f5832b186
alexks02 <alex.ks02@gmail.com>
2019-07-16 10:43:56 -0400

    Minor improvements

--- build_n_push.sh
+++ build_n_push.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-sudo docker-compose build && sudo docker-compose push && sudo docker-compose rm -f && sudo docker images -q | xargs docker rmi 2>/dev/null
+sudo docker-compose build && \
+sudo docker-compose push && \
+sudo docker-compose rm -f && \
+sudo docker images -q | xargs sudo docker rmi 2>/dev/null
